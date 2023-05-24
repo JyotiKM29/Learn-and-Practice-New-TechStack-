c23b37adb938841c2ad32341c8b057499ffe0b35
Jyoti KM <jyotikumari4442@gmail.com>
2023-05-24 17:56:50 +0530

    Add testimonal ,CTA and footer section in tailwindcss

--- 02 Tailwindcss/tailwind.config.js
+++ 02 Tailwindcss/tailwind.config.js
@@ -17,7 +17,7 @@ module.exports = {
         darkGrayishBlue: 'hsl(227, 12% ,61% )',
         veryDarkBlue: 'hsl(233, 12% ,13% )',
         veryPaleRed: 'hsl(13, 100% ,96% )',
-        veryLightGrey: 'hsl(0, 0% ,13% )',
+        veryLightGray: 'hsl(0, 0%, 93%);',
       }
     },
   },
